ab4f95e51753266df19c807b7a8b0f3e6a7e7ff2
Kovchin <kovchin@mail.ru>
2020-10-05 17:59:18 +0700

    start

---
+++ database/seeders/CarsSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Car;
+
+class CarsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Car::factory()
+            ->times(50)
+            ->create();
+    }
+}
